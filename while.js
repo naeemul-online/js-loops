d576c7bbbc7c6f6144bb12678f35eaa2db1a3990
naeemul-online <naeemul.online@gmail.com>
2024-01-29 21:46:09 +0300

    first commit

---
+++ while.js
@@ -0,0 +1,48 @@
+//while loop
+/* 
+1. Create a while loop
+let num = 1;
+while(num <= 10){
+    console.log(num);
+    num++;
+}
+-----------------------------------------
+2. Print 1 - 10;
+let num = 1;
+while(num <= 10){
+    console.log(num);
+    num++;
+}
+------------------------------------------
+3. Print Even Number in between 1 to 10;
+let num = 1;
+while(num <= 10){
+    // console.log(num);
+    if(num%2 == 0) {
+        console.log(num)
+    }
+    num++;
+}
+--------------------------------------------
+4. Print Odd Number in Between 1 to 10;
+let num = 1;
+while(num <= 10){
+    // console.log(num);
+    if(num%2 == 1) {
+        console.log(num)
+    }
+    num++;
+}
+-------------------------------------------
+
+5. Print sum of 1 to 10;
+let num = 1;
+let sum = 0;
+while(num <= 100){
+    sum += num;
+    num++;
+
+}
+console.log(sum)
+-----------------------------------------------
+*/
